21581d95a68549b70d004d6c8677187eca6cf32f
MadhavPolawala-dds <madhavpolawala.dds@gmail.com>
2024-08-30 18:06:44 +0530

    horoscope component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { HoroscopeComponent } from './pages/horoscope/horoscope.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'horoscope',
+    component:HoroscopeComponent
+  }
+];
--- src/app/pages/horoscope/horoscope.component.ts
+++ src/app/pages/horoscope/horoscope.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import jsonData from '../../../assets/data/horoscope.json';
 
 @Component({
   selector: 'app-horoscope',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './horoscope.component.html',
-  styleUrl: './horoscope.component.css'
+  styleUrl: './horoscope.component.css',
 })
 export class HoroscopeComponent {
+  selectedItemId: number | null = null;
 
+  horoscopes: any = jsonData.horoscope;
+
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef;
+
+  scrollLeft() {
+    if (this.scrollContainer) {
+      const container = this.scrollContainer.nativeElement;
+      container.scrollLeft -= 182;
+      console.log('Scrolled left:', container.scrollLeft);
+    }
+  }
+
+  scrollRight() {
+    if (this.scrollContainer) {
+      const container = this.scrollContainer.nativeElement;
+      container.scrollLeft += 182;
+      console.log('Scrolled right:', container.scrollLeft);
+    }
+  }
+
+  selectItem(itemId: number) {
+    this.selectedItemId = itemId;
+    console.log('Selected item ID:', itemId);
+  }
 }
