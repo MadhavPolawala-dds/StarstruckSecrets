cb7b396ddbb1b1677a21908c359081bb43e85d08
MadhavPolawala-dds <madhavpolawala.dds@gmail.com>
2024-09-16 09:23:32 +0530

    more component responsive

--- src/app/pages/coffee-cup-details/coffee-cup-details.component.ts
+++ src/app/pages/coffee-cup-details/coffee-cup-details.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { MoreComponent } from '../../components/more/more.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-coffee-cup-details',
   standalone: true,
-  imports: [HeaderComponent, MoreComponent, FooterComponent],
+  imports: [HeaderComponent, MoreComponent, FooterComponent, RouterLink],
   templateUrl: './coffee-cup-details.component.html',
   styleUrl: './coffee-cup-details.component.css',
 })
